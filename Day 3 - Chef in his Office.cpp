d0137a2b4d7653a27da382749434ce25e858f75a
M33N4k5Hi <code.meenakshimishra@gmail.com>
2024-09-30 21:14:00 +0530

    Create Day 3 - Chef in his Office.cpp

---
+++ Day 3 - Chef in his Office.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n,a,b;
+	// your code goes here
+	scanf("%d", &n);
+	while(n!=0){
+	    scanf("%d" "%d", &a,&b);
+	    int temp=(a*4)+b;
+	    printf("%d\n", temp);
+	    n--;
+	}
+    
+}
