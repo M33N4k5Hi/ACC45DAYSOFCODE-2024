992d5d998f2be3959a81fae3a9d620ad3f9c9533
M33N4k5Hi <code.meenakshimishra@gmail.com>
2024-10-09 23:35:47 +0530

    Add files via upload

---
+++ Day 12.FLIPCARDS.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int t,x,n;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d%d",&n,&x);
+	    if(x<(n-x)){
+	        printf("%d\n",x);
+	    }
+	    else{
+	        printf("%d\n",(n-x));
+	    }
+	}
+	return 0;
+}
+
