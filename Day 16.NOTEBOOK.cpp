d4116bb91cede957073aaf5af6ccd297178d3cc4
M33N4k5Hi <code.meenakshimishra@gmail.com>
2024-10-13 23:44:12 +0530

    Add files via upload

---
+++ Day 16.NOTEBOOK.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T; 
+
+    for (int i = 0; i < T; i++) {
+        int N; 
+        cin >> N;
+
+        int notebooks = N * 10; 
+        cout << notebooks << endl; 
+    }
+
+    return 0;
+}
