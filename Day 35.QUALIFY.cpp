f94474332fb0d7711d1a0140509878639f8043a6
M33N4k5Hi <code.meenakshimishra@gmail.com>
2024-11-06 23:08:28 +0530

    Add files via upload

---
+++ Day 35.QUALIFY.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, A, B;
+        cin >> X >> A >> B;
+
+        int totalScore = A + 2 * B;
+
+        if (totalScore >= X) {
+            cout << "Qualify" << endl;
+        } else {
+            cout << "NotQualify" << endl;
+        }
+    }
+
+    return 0;
+}
